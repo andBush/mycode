3a39321ed45adbcf78150ce8b09d15e3a8f0d377
andBush <bushin-08@mail.ru>
2024-09-29 18:00:54 +0300

    fixed user

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import User, Vacancy, Employer
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Vacancy)
+admin.site.register(Employer)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+
+class User(get_user_model()):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=50)
+    age = models.IntegerField(default=18)
+
+
+class Employer(models.Model):
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=250, null="", blank=True)
+
+
+class Vacancy(models.Model):
+    def __str__(self):
+        return f"{self.author}'s vacancy {self.id}"
+    author = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    salary_per_month = models.IntegerField()
+
